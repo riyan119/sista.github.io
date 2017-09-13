0403a91f8e723f43276769a1fadbad2a2e1a845c
Riyan Setiawan <muhammadriyansetiawan@gmail.com>
2017-09-13 09:32:41 +0700

    Sista Web Responsive

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HTML5/CSS3 RESPONSIVE THEME</title>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" type="text/css" href="inc/style.css">
+	<meta name="viewport" content="width=device-width, intial-scale=1.0">
+</head>
+<body class="body">
+ <header class="mainheader">
+ <img src="img/header.jpg">
+ <nav>
+ 	<ul>
+ 		<li class="active"><a href="">Home</a></li>
+ 		<li><a href="">About</a></li>
+ 		<li><a href="">Portofolio</a></li>
+ 		<li><a href="">Contact</a></li>
+ 	</ul>
+ </nav>
+ </header>
+
+ <div class="maincontent">
+ 	<div class="content">
+ 		<article class="topcontent">
+ 			<header>
+ 				<h2><a href="" title="First Post">Firtst Post</a></h2>
+ 			</header>
+
+ 			<footer>
+ 				<p class="post-info">This post is written by riyanriyans</p>
+ 			</footer>
+
+ 			<content>
+ 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+ 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+ 				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+ 				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+ 				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+ 				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+ 				<p>
+ 					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+ 					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+ 					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+ 					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+ 					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+ 					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+ 				</p>
+ 			</content>
+ 		</article>
+
+ 		<article class="bottomcontent">
+ 			<header>
+ 				<h2><a href="" title="Second Post">Second Post</a></h2>
+ 			</header>
+
+ 			<footer>
+ 				<p class="post-info">This post is written by riyanriyans</p>
+ 			</footer>
+
+ 			<content>
+ 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+ 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+ 				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+ 				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+ 				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+ 				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+ 				<p>
+ 					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+ 					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+ 					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+ 					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+ 					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+ 					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+ 				</p>
+ 			</content>
+ 		</article>
+ 	</div>
+ </div>
+
+ <aside class="top-sidebar">
+ 	<article>
+ 		<h2>Top Sidebar</h2>
+ 		<p>Muhammad RIyan Setiawan yang sedang belajar menjadi programer</p>
+ 	</article>
+ </aside>
+
+ <aside class="middle-sidebar">
+ 	<article>
+ 		<h2>Middle Sidebar</h2>
+ 		<p>Muhammad RIyan Setiawan yang sedang belajar menjadi programer</p>
+ 	</article>
+ </aside>
+
+ <aside class="bottom-sidebar">
+ 	<article>
+ 		<h2>Bottom Sidebar</h2>
+ 		<p>Muhammad RIyan Setiawan yang sedang belajar menjadi programer</p>
+ 	</article>
+ </aside>
+
+ <footer class="mainfooter">
+ 	<p>Copyright &copy; <a href="" title="riyanriyans.pe.hu">riyanriyans.pe.hu</a></p>
+ </footer>
+</body>
+</html>
